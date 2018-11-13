15304fc0dcff604b445e623a1d3f869d35c72c69
GapurEvloev <kaloycant@gmail.com>
2018-11-13 08:26:53 +0300

    two commit

---
+++ dev/static/js/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+    svg4everybody({});
+
+    let mainSubnavHover = () => {
+    	$('.main-subnav__item').hover(
+    		function() {
+    			let parentList = $(this).closest('.main-subnav__list');
+    			if ($(this).children('.main-subnav__list').length) {
+    				let catNavHeight = $(this).children('.main-subnav__list').outerHeight();
+    				if (parentList.outerHeight() < catNavHeight) {
+    					parentList.css('height', catNavHeight);
+    				}
+    				parentList.css('width', '720');
+    			}
+
+    		},
+    		function() {
+    			let parentList = $(this).closest('.main-subnav__list');
+    			parentList.css('height', 'auto')
+    			parentList.css('width', 'auto')
+    		}
+    	)
+    };
+    let openSearchForm = () =>{
+        $(document).on('click','.search__icon',function(){
+            $(this).parent().addClass('search--open');
+        });
+    };
+    let clearSearchForm = () => {
+        $(document).on('click','.search__clear',function(){
+            $('.search__input').val('');
+        });
+    };
+
+    let bannerSlider = () => {
+    	$('.js-banner').slick({
+			slideToShow: 1,
+			slidesToScroll: 1,
+			prevArrow: ".banner__navigation--prev",
+			nextArrow: ".banner__navigation--next",
+			dots: true,
+            customPaging : function(slider, i) {
+                return '<a class="banner__dot"></a>';
+            },
+			appendDots: '.banner__dots'
+		})
+	};
+
+    mainSubnavHover();
+    openSearchForm();
+    clearSearchForm();
+    bannerSlider();
+});
