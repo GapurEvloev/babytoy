e00b7c30cb53397a0cbbcf8894705db3fa1c3b25
GapurEvloev <kaloycant@gmail.com>
2018-11-28 00:28:41 +0300

    lsjdf

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -114,4 +114,44 @@ $(document).ready(function () {
     productPrevSlider();
     productLineSlider();
 
+}); 
+
+$(window).on('load', function(){
+    $(".sk-circle").fadeOut();
+    $(".preloader").delay(100).fadeOut("slow");
+    $("body").removeClass("fixed");
 });
+// полифилы для ie11 
+(function() {
+
+  // проверяем поддержку
+  if (!Element.prototype.closest) {
+
+    // реализуем
+    Element.prototype.closest = function(css) {
+      var node = this;
+
+      while (node) {
+        if (node.matches(css)) return node;
+        else node = node.parentElement;
+      }
+      return null;
+    };
+  }
+
+})();
+
+(function() {
+
+  // проверяем поддержку
+  if (!Element.prototype.matches) {
+
+    // определяем свойство
+    Element.prototype.matches = Element.prototype.matchesSelector ||
+      Element.prototype.webkitMatchesSelector ||
+      Element.prototype.mozMatchesSelector ||
+      Element.prototype.msMatchesSelector;
+
+  }
+
+})();
